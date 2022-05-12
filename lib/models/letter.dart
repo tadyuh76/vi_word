88fbd11bb77c60e15fd804373b8b912052ba69cc
tadyuh76 <tadyuh76@gmail.com>
2022-05-12 14:09:43 +0700

    complete basic

---
+++ lib/models/letter.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:vi_word/utils/colors.dart';
+
+enum LetterStatus { initial, notInWord, wrongPosition, wrongAccent, correct }
+
+class Letter {
+  String val;
+  LetterStatus status;
+
+  Letter({required this.val, this.status = LetterStatus.initial});
+
+  factory Letter.empty() => Letter(val: '');
+
+  Color get backgroundColor {
+    switch (status) {
+      case LetterStatus.initial:
+        return Colors.transparent;
+      case LetterStatus.correct:
+        return primary;
+      case LetterStatus.wrongAccent:
+        return secondary;
+      case LetterStatus.wrongPosition:
+        return ternary;
+      case LetterStatus.notInWord:
+        return Colors.grey[700]!;
+      default:
+        return Colors.transparent;
+    }
+  }
+
+  Color get borderColor {
+    switch (status) {
+      case LetterStatus.initial:
+        return Colors.grey;
+      default:
+        return Colors.transparent;
+    }
+  }
+
+  void copyWith({String? val, LetterStatus? status}) {
+    this.val = val ?? this.val;
+    this.status = status ?? this.status;
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color(0xFF0CC078);
+const Color secondary = Color(0xFFA8E4EF);
+const Color ternary = Color(0xFFFCFC99);
+const Color red = Color(0xFFFB6862);
